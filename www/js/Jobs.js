48ab90d4dafaae30342b35b684f2e8f7a9fd477d
jonathandenby <jon_denby@hotmail.com>
2013-12-04 16:36:12 +0000

    k

--- www/js/Jobs.js
+++ www/js/Jobs.js
@@ -1,21 +1,71 @@
 ﻿var jobs = {
 
+
+
     getJobs: function () {
+
+
+        /*
         var jobs = [];
+        var term = {};
+        jQuery.support.cors = true;
 
-        job = new Object();
-        job.JobNumber = '1111111';
-        job.MovementNumber = '111111111';
+        alert('a');
+        jQuery.getJSON("http://www.c4logistics.com/userdata/CurrentLiveForCarrier.aspx?callback=?",
+        null,function (data) {
+        alert("dd");
+        });
+        alert('a');
 
-        jobs.push(job);
-        
-                job = new Object();
-        job.JobNumber = '2222222';
-        job.MovementNumber = '222222222';
 
-        jobs.push(job);
 
+ 
         return jobs;
-    }
+        }
+        }
 
+
+
+        alert('a');
+        downloadUrl("http://www.c4logistics.com/userdata/CurrentLiveForCarrier.aspx", function (doc) {
+        alert('b');
+        var xml = xmlParse(doc);
+        var movements = xml.documentElement.getElementsByTagName("movement");
+
+
+        for (var i = 0; i < movements.length; i++) {
+        job = new Object();
+        job.MovementNumber = movements[i].getAttribute("MovementNumber");
+        jobs.push(job);
+        }
+        });
+        */
+
+    }
 }
+
+var lat = 0;
+var lng = 0;
+//A button click will call this function
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(onSuccess, onError, { enableHighAccuracy: true });
+}
+
+// onSuccess Geolocation
+//
+function onSuccess(position) {
+    //Lat long will be fetched and stored in session variables
+    //These variables will be used while storing data in local database 
+    lat = position.coords.latitude;
+    lng = position.coords.longitude;
+    alert('Lattitude: ' + lat + ' Longitude: ' + long);
+    sessionStorage.setItem('lattitude', lat);
+    sessionStorage.setItem('longitude', lng);
+}
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+    alert('code: ' + error.code + '\n' +
+          'message: ' + error.message + '\n');
+}
+ 
