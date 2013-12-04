224211ee7e1f649c2f9d411b3b72f44f146fd37f
jonathandenby <jon_denby@hotmail.com>
2013-12-04 10:16:18 +0000

    kk

---
+++ www/js/Jobs.js
@@ -0,0 +1,21 @@
+﻿var jobs = {
+
+    getJobs: function () {
+        var jobs = [];
+
+        job = new Object();
+        job.JobNumber = '1111111';
+        job.MovementNumber = '111111111';
+
+        jobs.push(job);
+        
+                job = new Object();
+        job.JobNumber = '2222222';
+        job.MovementNumber = '222222222';
+
+        jobs.push(job);
+
+        return jobs;
+    }
+
+}
